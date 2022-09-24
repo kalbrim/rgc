e4584e2dbf8ecf1efc991f60d0ace2ad01bf9ac9
Brendan Comiskey <comiskeybrendan@gmail.com>
2022-09-24 11:46:17 -0400

    another out line

---
+++ src/GolfClub.java
@@ -0,0 +1,30 @@
+public abstract class GolfClub {
+
+    String grip = "";
+    String shaft = "";
+    String head = "";
+
+    public String getGrip() {
+        return grip;
+    }
+
+    public void setGrip(String grip) {
+        this.grip = grip;
+    }
+
+    public String getShaft() {
+        return shaft;
+    }
+
+    public void setShaft(String shaft) {
+        this.shaft = shaft;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+}
